3a5261fb57c94ea7b5de54b2b71a7658aaaacd28
Артём Буевич <coud44@gmail.com>
2025-05-24 20:07:43 +0300

    Initial commit

---
+++ src/main/java/com/testtask/tesktask/TesktaskApplication.java
@@ -0,0 +1,13 @@
+package com.testtask.tesktask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesktaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesktaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/testtask/tesktask/TesktaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testtask.tesktask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesktaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
